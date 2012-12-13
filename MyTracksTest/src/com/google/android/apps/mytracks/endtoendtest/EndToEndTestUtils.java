8dc6e2f8d7a9f8acffccaa1e249869f74b2b7882
youtaol <youtaol@localhost>
2012-12-13 17:02:18 +0800

    Add test case to collect the memory/battery usage of MyTracks.

--- MyTracksTest/src/com/google/android/apps/mytracks/endtoendtest/EndToEndTestUtils.java
+++ MyTracksTest/src/com/google/android/apps/mytracks/endtoendtest/EndToEndTestUtils.java
@@ -92,6 +92,7 @@ public class EndToEndTestUtils {
   public static String KM = "";
   public static String MILE = "";
   
+  public static int TINY_WAIT_TIME = 2000;
   public static int SHORT_WAIT_TIME = 2000;
   public static int NORMAL_WAIT_TIME = 8000;
   public static int LONG_WAIT_TIME = 15000;
